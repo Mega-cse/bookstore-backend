5542bf7621b1463e3937eebc6051e25524cf4698
megavardhini1958@gmail.com <megavardhini1958@gmail.com>
2026-02-18 03:40:19 +0530

    Added env setup

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 
-app.use("/api", require("./routes/authRoutes"));
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/books", require("./routes/bookRoutes"));
-//app.use("/api/orders", require("./routes/orderRoutes"));
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
